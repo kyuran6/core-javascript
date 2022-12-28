e3b63dc68f99c39f82d187942f3fad93670a5eb1
kyran6 <loveheee@gmail.com>
2022-12-28 13:33:32 +0900

    [추가] globalthis, datatypes

---
+++ client/chapter/02.variables.js
@@ -0,0 +1,55 @@
+/* --------------------------------------------------------------------- */
+/* Variables                                                             */
+/* --------------------------------------------------------------------- */
+
+
+/* eslint no-unused-vars:0 */
+let admin;
+let name;
+
+name = 'John';
+admin = name
+
+console.log(admin);
+
+현재 접속중인 사용자
+
+let ourPlanetName = 'Earth'
+let currentUserName = 'kindtiger'
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365
+
+// - 구매 결제 여부
+let isPaymeny = false;
+let hasClassName = false
+
+
+// - 구매 결제 내역
+let paymentHistory
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'EUID';
+
+// - 오늘의 운세
+const productInfo = '';
+
+// - 상품 정보
+const productInformation = '';
+
+/* variables ----------------------------------------------------------- */
+
+
+/* constant variables -------------------------------------------------- */
+
+/* Lexical
+Enviroment
+*/
+
+var 
