eed88af465a07355df3a01f49c620dc0602f82ed
kyran6 <loveheee@gmail.com>
2022-12-29 15:58:37 +0900

    조건문

--- client/chapter/core/07.operation.js
+++ client/chapter/core/07.operation.js
@@ -2,52 +2,65 @@
 /* Operators                                                              */
 /* ---------------------------------------------------------------------- */
 
+let firstValue = Number(prompt("덧셈할 첫 번째 숫자를 입력해주세요.", 10));
+let secondValue = +prompt("덧셈할 두 번째 숫자를 입력해주세요.",2);
+
+// console.log(firstValue+secondValue);
+console.log(typeof secondValue);
+
 
 // 연산자(演算子): 연산을 표시하기 위한 기호
 // 피연산자(被演算子): 처리 대상
+let a = '10';
+let b = '30';
 
 // 단항 연산자
-let unary;
+let unary = +a;
 
 // 이항 연산자
-let binary;
+let binary = a + b;
 
 // 삼항 연산자
-let ternary;
+let ternary = Math.random()>0.5 ? 'big':'small';
 
 // 산술 연산자: 덧셈
-let addition;
+let addition = 5 + 3;
 
 // 산술 연산자: 뺄셈
-let subtraction;
+let subtraction = 8 - 2;
 
 // 산술 연산자: 곱셈
-let multiplication;
+let multiplication = 2*3;
 
 // 산술 연산자: 나눗셈
-let division;
+let division = 10/5;
 
 // 산술 연산자: 나머지
-let remainder;
+let remainder = 10%3;
 
 // 산술 연산자: 거듭 제곱
-let power;
+let power = 2 ** 53;
 
 
 // JavaScript 연산자는 피연산자를 적절한 타입(유형)으로 강제 변환합니다.
 let coercionTypeConversion = '9' * '3';
 
 // 대부분의 연산자는 기본 값으로만 작동합니다.
-let onlyWorkDefaultValues = [1,2,3] + [4,5,6];
+let onlyWorkDefaultValues = [... firstArray, ... secondArray,8,9,10];
 
+console.log(onlyWorkDefaultValues);
 
 // 연산자 우선 순위
 // 단항(+,-) > 거듭제곱(**) > 곱셈(*) > 나눗셈(/) > 덧셈(+) > 뺄셈(-) > 할당(=)
 
+let number=100;
 
 // 선,후 증감 연산자
 // ++, --
-
+let number: number
+number++; //100
+++number;
+console.log(number);
 
 // 아래 코드를 읽기 쉽도록 변경합니다.
 // 그리고 연산자 우선 순위에 따라 연산 과정을 유추해보세요.
