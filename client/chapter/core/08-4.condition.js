f7b931832a87f8d5e56e9662f5ea48e60c29ca5f
kyran6 <loveheee@gmail.com>
2022-12-30 11:08:29 +0900

    core

--- client/chapter/core/08-4.condition.js
+++ client/chapter/core/08-4.condition.js
@@ -14,9 +14,24 @@ if (emailAddress === undefined || emailAddress === null) {
 // 3항 연산자 (ternary) 를 사용한 식으로 변경합니다.
 
 
+
 // 위 조건 처리문을 nullish 병합 연산자를 사용한 식으로 변경합니다.
 
 
 /* ?? vs. || ----------------------------------------------------------- */
 // || → 첫번째 Truthy 값을 반환
 // ?? → 첫번째 정의된(defined) 값을 반환
+
+const WIDTH = '10px'
+
+console.log( null || WIDTH );
+console.log( null ?? WIDTH );
+
+console.log( undefined || WIDTH );
+console.log( undefined ?? WIDTH );
+
+console.log( false || WIDTH );
+console.log( false ?? WIDTH );
+
+console.log( '' || WIDTH );
+console.log( '' ?? WIDTH );
