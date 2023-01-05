90c4929d8358303abaebc18ea57da9001cf2f28a
kyran6 <loveheee@gmail.com>
2023-01-05 11:25:06 +0900

    [추가] 클로저 완전정복

--- client/chapter/core/09-1.loop.js
+++ client/chapter/core/09-1.loop.js
@@ -2,6 +2,27 @@
 /* While Loop                                                             */
 /* ---------------------------------------------------------------------- */
 
+/* 
+let i = 0;
+
+while(i < 10){
+  console.log(++i);
+
+  
+}
+
+ */
+
+
+
+
+
+
+
+
+
+
+
 
 const frontEndDev = [
   'HTML',
@@ -11,18 +32,74 @@ const frontEndDev = [
   'jQuery',
   'React',
   'Redux',
-  'asdas',
-  'asdasdasd'
 ];
 
 /* 프론트엔드 개발 집합 항목 출력 ---------------------------------------------- */
 
-console.log(frontEndDev[0]);
-console.log(frontEndDev[1]);
-console.log(frontEndDev[2]);
-console.log(frontEndDev[3]);
-console.log(frontEndDev[4]);
-console.log(frontEndDev[5]);
+// console.log(frontEndDev[0]);
+// console.log(frontEndDev[1]);
+// console.log(frontEndDev[2]);
+// console.log(frontEndDev[3]);
+// console.log(frontEndDev[4]);
+// console.log(frontEndDev[5]);
+
+
+let z1 = performance.now();
+
+let i = 0;
+
+while(i < frontEndDev.length){
+
+  let value = frontEndDev[i];
+
+  // console.log(i);
+
+  console.log(value);
+
+  i++;
+}
+
+
+let z2 = performance.now();
+
+console.log(z2-z1);
+
+
+
+/* 
+let i = frontEndDev.length;
+
+while( i >= 0){
+
+  console.log(i, frontEndDev[i] );
+
+  i--
+}
+
+ */
+
+// 전개 연산자  전개 구문 spread oparator
+
+// let copyArray = [...frontEndDev];
+
+
+
+
+// let copyArray = frontEndDev.slice();
+
+
+// while(copyArray.length){
+//   console.log(copyArray.pop());
+// }
+
+
+
+
+
+
+
+
+
 
 
 /* 프론트엔드 개발 집합을 순환해서 각 아이템을 Console 패널에 출력 -------------------- */
@@ -33,16 +110,11 @@ console.log(frontEndDev[5]);
 
 // 성능 진단 : 순환 vs. 역순환
 
-/*
-let i = 0;
 
-while( i < frontEndDev.length){
-  let value = frontEndDev[i];
-  //console.log(i);
-  console.log(value);
-  i++;
-}
-*/
+
+
+
+
 
 
 
